0fb35eb2a4bf83016584e28a47d1ceb294b0108b
jasonrado <70185340+jasonrado@users.noreply.github.com>
2021-05-28 16:36:26 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
 	user: "wtlijjbmmgepbr",
-	password: "30f84d7c05f774110a3df4a17af996126797a4b173a820e900fcbea6c7026558g",
+	password: "30f84d7c05f774110a3df4a17af996126797a4b173a820e900fcbea6c7026558",
 	host: "ec2-34-202-54-225.compute-1.amazonaws.com",
 	port: 5432,
 	database: "d5qcbcgmrtdev9",
