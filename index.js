0a59774a7ec65a78653f04a09841f61397c07739
jasonrado <70185340+jasonrado@users.noreply.github.com>
2021-05-28 17:06:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const app = express();
 const cors = require("cors");
 const pool = require ("./db");
+const { json } = require("express");
 
 //middleware
 app.use(cors());
